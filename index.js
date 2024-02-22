db017f9cecb07a74e2801c4043d594fd2aabee2a
Dalton Riehl <dtonriehl@gmail.com>
2024-02-21 19:25:42 -0600

    Functions Continued Lab Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(decision) {
+    const message = "This Saturday, I want to "
+    switch(decision) {
+        case 'bathe my dog':
+            return message + "bathe my dog!";
+        default:
+            return message + "roller-skate!";
+    }
+}
+
+function mondayWork(choice) {
+    const message = "This Monday, I will ";
+    switch(choice) {
+        case 'work from home':
+            return message + "work from home.";
+        default:
+            return message + "go to the office.";
+    }
+}
+
+function wrapAdjective(death = "*") {
+    return function endMy(suffering = "special") {
+       return "You are " + death + suffering + death + "!";
+    }
+}
